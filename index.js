48e79ef110b93a53f66dd1aa6101fb18b0ca54fa
Zach <zacharyeverett.00@gmail.com>
2024-03-22 23:22:29 +1100

    fixed directories

--- index.js
+++ index.js
@@ -290,10 +290,10 @@ for (let i = 0; i < 6; i++) {
 addAggregateRow()
 addAtarEstimateRow()
 
-fetch("/ScalingReport.json").then((response) => response.json()).then((data) => {
+fetch("./ScalingReport.json").then((response) => response.json()).then((data) => {
     scalingReport = data
 })
-fetch("/AggregateToAtar.json").then((response) => response.json()).then((data) => {
+fetch("./AggregateToAtar.json").then((response) => response.json()).then((data) => {
     aggregateToAtar = data
 })
 

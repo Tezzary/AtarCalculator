cd7431870eb5c629abe6871a80e9aa7588f4ef07
Tezzy <zacharyeverett.00@gmail.com>
2024-03-22 15:54:58 +1100

    to pc

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ let dropdown = undefined
 
 let subjects = []
 
+let aggregateRowCells = []
+
 class Subject{
     constructor(compulsoryEnglish=false){
         this.cells = []
-        this.rawScore = 0
+        this.rawScore = 30
         this.scaledSS = []
         this.compulsoryEnglish = compulsoryEnglish
     }
 }
-function getValidSubjects(text, englishOnly=false){
+function getValidSubjects(textFilter, englishOnly=false){
     let yearSubjects = scalingReport["2023"]
 
     let validSubjects = []
@@ -28,7 +30,7 @@ function getValidSubjects(text, englishOnly=false){
         if(englishOnly && !(subject.subjectName.toLowerCase().includes("english") || subject.subjectName.toLowerCase().includes("literature"))){
             continue
         }
-        if(subject.subjectName.toLowerCase().includes(text.toLowerCase())){
+        if(subject.subjectName.toLowerCase().includes(textFilter.toLowerCase())){
             validSubjects.push(subject)
         }
     }
@@ -37,7 +39,6 @@ function getValidSubjects(text, englishOnly=false){
 function addDropDownItem(text, callback){
     let subjectDiv = document.createElement("div")
     subjectDiv.innerText = text
-    subjectDiv.classList.add("dropdown-item")
     subjectDiv.addEventListener("click", (event) => {
         callback(event)
     })
@@ -80,45 +81,64 @@ function handleTyping(text, input, subjectObj){
         
     }
 }
-function updateScaledScores(subjectObj){
-    if (subjectObj.subject == undefined){
-        return
-    }
-    for(let i = 2; i < subjectObj.cells.length; i++){
-        let cell = subjectObj.cells[i]
-        let year = 2019 + i - 2
-        let subject = scalingReport[year].find(subject => subject.subjectName == subjectObj.subject.subjectName)
-        let baseline = [20, 25, 30, 35, 40, 45, 50]
-        for(let j = 0; j < baseline.length; j++){
-            if(subjectObj.rawScore <= baseline[j]){
-
-                if (j == 0){
-                    let scaledSS = subjectObj.rawScore - (baseline[j] - subject.subjectScalingData[j])
+function populatePage(){
+    for(let k = 0; k < subjects.length; k++){
+        let subjectObj = subjects[k]
+        if(subjectObj.subject == undefined){
+            continue
+        }
+        subjectObj.scaledSS = []
+        for(let i = 2; i < subjectObj.cells.length; i++){
+            let cell = subjectObj.cells[i]
+            let year = 2019 + i - 2
+            let subject = scalingReport[year].find(subject => subject.subjectName == subjectObj.subject.subjectName)
+            let baseline = [20, 25, 30, 35, 40, 45, 50]
+            for(let j = 0; j < baseline.length; j++){
+                if(subjectObj.rawScore <= baseline[j]){
+    
+                    if (j == 0){
+                        let scaledSS = subjectObj.rawScore - (baseline[j] - subject.subjectScalingData[j])
+                        if (scaledSS < 0){
+                            scaledSS = 0
+                        }
+                        cell.innerText = scaledSS
+                        break
+                    }
+                    let offset = subjectObj.rawScore - baseline[j - 1]
+                    let diff = baseline[j] - baseline[j - 1]
+                    let percent = offset / diff
+                    console.log(percent)
+                    let scaledSS = subject.subjectScalingData[j - 1] + percent * (subject.subjectScalingData[j] - subject.subjectScalingData[j - 1])
                     if (scaledSS < 0){
                         scaledSS = 0
                     }
+                    subjectObj.scaledSS.push(scaledSS)
                     cell.innerText = scaledSS
                     break
                 }
-                let offset = subjectObj.rawScore - baseline[j - 1]
-                let diff = baseline[j] - baseline[j - 1]
-                let percent = offset / diff
-                console.log(percent)
-                let scaledSS = subject.subjectScalingData[j - 1] + percent * (subject.subjectScalingData[j] - subject.subjectScalingData[j - 1])
-                if (scaledSS < 0){
-                    scaledSS = 0
-                }
-                cell.innerText = scaledSS
-                break
             }
         }
     }
+    for(let i = 0; i < aggregateRowCells.length; i++){
+        let cell = aggregateRowCells[i]
+        let sum = 0
+        for(let j = 0; j < subjects.length; j++){
+            let subjectObj = subjects[j]
+            if(subjectObj.subject == undefined){
+                continue
+            }
+            let scaledSS = subjectObj.scaledSS[i]
+            sum += scaledSS
+        }
+        cell.innerText = sum
+    }
+    
 }
 function handleClick(input, subject, subjectObj){
     subjectObj.subject = subject
     dropdown.remove()
     input.value = subject.subjectName
-    updateScaledScores(subjectObj)
+    populatePage()
 }
 function handleTextActivation(event, subjectObj){
     let input = event.target
@@ -132,8 +152,9 @@ function handleTextActivation(event, subjectObj){
         })
     }
 }
-function handleColumn(index, cell, subjectObj){
-    switch (index){
+function createSubjectCell(columnIndex, cell, subjectObj){
+    switch (columnIndex){
+        //add subject selector
         case 0:
             let input = document.createElement("input")
             input.type = "text"
@@ -146,24 +167,26 @@ function handleColumn(index, cell, subjectObj){
             })
             cell.appendChild(input)
             break
+        //add raw score input
         case 1:
             let rangeField = document.createElement("input")
             rangeField.type = "number"
             rangeField.min = 0
             rangeField.max = 50
-            rangeField.value = 0
+            rangeField.value = 30
             rangeField.addEventListener("input", (event) => {
                 subjectObj.rawScore = event.target.value
-                updateScaledScores(subjectObj)
+                populatePage()
             })
             cell.appendChild(rangeField)
             break
+        //add scaled score cells for all other cells
         default:
             cell.innerText = "-"
     }
 
 }
-function addRow(compulsoryEnglish){
+function addSubjectRow(compulsoryEnglish){
     //let row = document.createElement("div")
     //row.classList.add("row")
     rows.push([])
@@ -172,7 +195,7 @@ function addRow(compulsoryEnglish){
     for (let i = 0; i < elementsPerRow; i++) {
         
         let cell = document.createElement("div")
-        handleColumn(i, cell, subjectObj)
+        createSubjectCell(i, cell, subjectObj)
         rows[rows.length-1].push(cell)
         cell.classList.add("cell")
         container.appendChild(cell)
@@ -180,18 +203,35 @@ function addRow(compulsoryEnglish){
         
     }
 }
-function addHeader(headers){
+function addStaticRow(headers){
     for(let i = 0; i < headers.length; i++){
         let cell = document.createElement("div")
         cell.innerText = headers[i]
         cell.classList.add("cell")
+        cell.classList.add("headerCells")
         container.appendChild(cell)
     }
 }
-addHeader(["", "Raw Score", "2019 Scaled SS", "2020 Scaled SS", "2021 Scaled SS", "2022 Scaled SS", "2023 Scaled SS"])
-for (let i = 0; i < 6; i++) {
-    addRow(i==0)
+function addAggregateRow(){
+
+    for(let i = 0; i < elementsPerRow; i++){
+        let cell = document.createElement("div")
+        cell.classList.add("cell")
+        if(i==1){
+            cell.innerText = "Aggregate"
+        }
+        container.appendChild(cell)
+        if(i > 1){
+            aggregateRowCells.push(cell)
+        }  
+    }
 }
+addStaticRow(["", "Raw Score", "2019 Scaled SS", "2020 Scaled SS", "2021 Scaled SS", "2022 Scaled SS", "2023 Scaled SS"])
+for (let i = 0; i < 6; i++) {
+    //first row is compulsory english so i==0 is true for first column
+    addSubjectRow(i==0)
+}
+addAggregateRow()
 
 fetch("/ScalingReport.json").then((response) => response.json()).then((data) => {
     scalingReport = data
